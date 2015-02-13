781d89ff0d7ee8cc5c7ad7d8e1981673b3caf371
Taylor Young <taylor@hyprtxt.com>
2015-02-13 10:46:03 -0600

    nginx reporter

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+var glob = require('glob');
+
+/* GET home page. */
+router.get('/', function ( req, res ) {
+	var pileodata = {};
+
+	glob("*", { cwd: '/usr/local/etc/nginx/sites-enabled' }, function ( er, files ) {
+		pileodata.enabled = files;
+		glob("*", { cwd: '/usr/local/etc/nginx/sites-available' }, function ( er, files ) {
+			pileodata.available = files;
+			res.render('index', { title: '', files: pileodata });
+		})
+	})
+});
+
+router.get('/enabled', function ( req, res ) {
+	glob("*", { cwd: '/usr/local/etc/nginx/sites-enabled' }, function ( er, files ) {
+		res.render('index', { title: 'Sites Enabled', files: files });
+	})
+});
+
+router.get('/available', function ( req, res ) {
+	glob("*", { cwd: '/usr/local/etc/nginx/sites-available' }, function ( er, files ) {
+		res.render('index', { title: 'Sites Available', files: files });
+	})
+});
+
+module.exports = router;
